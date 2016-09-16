e85553fcfd8d05145d9f44b8f5c13d8b762ba900
DevinKWu <devink0430@gmail.com>
2016-09-17 02:53:03 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,2 @@
 console.log('This would be the main JS file.');
+$(document).ready(function(){alert("Hello World!");$.ajax({url:"params.json",method:"GET"}).done(function(msg){$("#main_content").empty().append(msg);});});
